4a96c6cd8c5196a453a4cf00c719d7ec6a5d5a24
Adrian Castillo Sanchez <xiuhblaze@gmail.com>
2022-05-23 12:00:57 -0500

    index.html actualizado

---
+++ docs/precache-manifest.e85415f547c2af50caee2938c235f6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b82ee12bf04a9b469eee",
+    "url": "/static/css/main.24fa5ed3.chunk.css"
+  },
+  {
+    "revision": "b82ee12bf04a9b469eee",
+    "url": "/static/js/main.b82ee12b.chunk.js"
+  },
+  {
+    "revision": "aba186b3b2061f1b7eae",
+    "url": "/static/css/1.cd7ae92d.chunk.css"
+  },
+  {
+    "revision": "aba186b3b2061f1b7eae",
+    "url": "/static/js/1.aba186b3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5107750cf676ff734ff13638bab74ba",
+    "url": "/index.html"
+  }
+];
